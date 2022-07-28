111af336a01b3e34bfc5b3456ae702014852fab7
Josphat Otieno <josphathumphrey5@gmail.com>
2022-07-28 17:47:23 +0300

    Update findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const newArr = [];
 
+    arr.forEach((obj) => {
+        if(!obj.hasOwnProperty('yearOfDeath')) {
+            newArr.push({name: obj.name,age: new Date().getFullYear() - obj.yearOfBirth});
+            
+        } else {
+            newArr.push({name: obj.name,age: obj.yearOfDeath - obj.yearOfBirth})
+        }
+    })
+
+    return newArr.sort((a,b) => b.age - a.age)[0];
+   
 };
 
 // Do not edit below this line
