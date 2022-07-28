7c39ff686ca39b647c0767db37a15f1ff5c042a1
Josphat Otieno <josphathumphrey5@gmail.com>
2022-07-28 15:47:31 +0300

    Update palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // console.log(str.split('').reverse().join('') === str);
+    const newStr = str.toLowerCase().match(/[a-z]/g).reverse().join('');
+    // console.log(newStr);
 
+    return newStr === str.toLowerCase().match(/[a-z]/g).join('');
 };
+// console.log(palindromes('racecar!'));
+
 
 // Do not edit below this line
 module.exports = palindromes;
