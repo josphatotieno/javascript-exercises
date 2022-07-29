eedaf0dcc054564c584ca97129f66b43a18a4ab3
Josphat Otieno <josphathumphrey5@gmail.com>
2022-07-29 09:52:09 +0300

    Update fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let arr = [1,1];
 
+    if(num < 0) {
+        return 'OOPS';
+    }
+
+        while(arr.length < num) {
+            const sum = arr[0]+arr[1];
+            arr.unshift(sum)
+        }
+    
+        return arr.reverse()[num-1];
+    
+
+    
 };
+
+// fibonacci(25)
 
 // Do not edit below this line
 module.exports = fibonacci;
